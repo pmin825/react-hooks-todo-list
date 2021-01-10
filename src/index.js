bf4cacb33304fc74af09e350703965ce672d69cc
pmin825 <pmin.0825@gmail.com>
2021-01-09 18:13:34 -0800

    create counter functions with usecontext and usereducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+export const UserContext = React.createContext();
+
+const username = "Dave";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <UserContext.Provider value={username}>
+        <App />
+    </UserContext.Provider>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
